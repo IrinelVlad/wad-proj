5f2820b7f6097b6e1309f0763fd23fa6223f33a8
IrinelVlad <irinel.vlad02@gmail.com>
2023-05-31 01:00:25 +0300

    DataLoader changes

--- springSecurity-final/readme.md
+++ springSecurity-final/readme.md
@@ -1,1 +1,11 @@
-Make sure to have in MySQL a database called "ecommerce"
+# F1 Team Customizer / Fantasy Career Mode
+* Vlad Irinel-Georgian / Musat Petru / Ghibea Ovidiu Catalin (1231EA)
+
+* Our web app will be a F1 Team Creator. The user will be able to customize his team from the ground-up (team name, livery, engine supplier, main sponsor, drivers,
+team principal, engineer team, home track). The user will have an allocated budget to create his team with, and will only have one team per account.
+
+* We will also try to implement a livery customizer, for the user to be able to design his dream F1 livery, including color schemes and previously selected sponsors.
+
+* All the drivers will have 'descriptions', containing their DoB, PoB, Best Race Finish and a short background. The user can choose from different storylines so that he could create the history of the team, depending on the the team name he chose (If he chooses a default team, he will have the team background. Otherwise, he will be able to select the story he likes best.)
+
+* We wish to implement a F1 Race Simulator at the end of the project (optional).
--- springSecurity-final/src/main/java/com/example/mvcproducts/bootstrap/DataLoader.java
+++ springSecurity-final/src/main/java/com/example/mvcproducts/bootstrap/DataLoader.java
@@ -29,6 +29,65 @@ public class DataLoader implements CommandLineRunner {
     //System.out.println(user2.getRoles().get(0).toString());
     userService.save(user2);
     Modification honda = new Modification("honda",Type.ENGINE,10);
+    Modification mercedes = new Modification("mercedes",Type.ENGINE,10);
+    Modification ferrari = new Modification("ferrari",Type.ENGINE,10);
+    Modification renault = new Modification("renault",Type.ENGINE,10);
+
+    Modification verstappen = new Modification("verstappen",Type.DRIVER,10);
+    Modification checo = new Modification("checo",Type.DRIVER,10);
+    Modification lewis = new Modification("lewis",Type.DRIVER,10);
+    Modification russel = new Modification("russel",Type.DRIVER,10);
+    Modification leclerc = new Modification("leclerc",Type.DRIVER,10);
+    Modification carlos = new Modification("carlos",Type.DRIVER,10);
+    Modification alonso = new Modification("alonso",Type.DRIVER,10);
+    Modification stroll = new Modification("stroll",Type.DRIVER,10);
+    Modification ocon = new Modification("ocon",Type.DRIVER,10);
+    Modification gasly = new Modification("gasly",Type.DRIVER,10);
+    Modification oscar = new Modification("oscar",Type.DRIVER,10);
+    Modification lando = new Modification("lando",Type.DRIVER,10);
+    Modification zhou = new Modification("zhou",Type.DRIVER,10);
+    Modification bottas = new Modification("bottas",Type.DRIVER,10);
+    Modification albon = new Modification("albon",Type.DRIVER,10);
+    Modification logan = new Modification("logan",Type.DRIVER,10);
+    Modification yuki = new Modification("yuki",Type.DRIVER,10);
+    Modification devries = new Modification("devries",Type.DRIVER,10);
+    Modification kmag = new Modification("kmag",Type.DRIVER,10);
+    Modification hulk = new Modification("hulk",Type.DRIVER,10);
+
+    Modification rbeng = new Modification("rbeng",Type.ENGINEER,10);
+    Modification merceng = new Modification("merceng",Type.ENGINEER,10);
+    Modification fereng = new Modification("fereng",Type.ENGINEER,10);
+    Modification mcleng = new Modification("mcleng",Type.ENGINEER,10);
+
+
     modificationService.save(honda);
+    modificationService.save(mercedes);
+    modificationService.save(ferrari);
+    modificationService.save(renault);
+    modificationService.save(verstappen);
+    modificationService.save(checo);
+    modificationService.save(lewis);
+    modificationService.save(russel);
+    modificationService.save(leclerc);
+    modificationService.save(carlos);
+    modificationService.save(alonso);
+    modificationService.save(stroll);
+    modificationService.save(ocon);
+    modificationService.save(gasly);
+    modificationService.save(oscar);
+    modificationService.save(lando);
+    modificationService.save(zhou);
+    modificationService.save(bottas);
+    modificationService.save(albon);
+    modificationService.save(logan);
+    modificationService.save(yuki);
+    modificationService.save(devries);
+    modificationService.save(kmag);
+    modificationService.save(hulk);
+    modificationService.save(rbeng);
+    modificationService.save(merceng);
+    modificationService.save(fereng);
+    modificationService.save(mcleng);
+
   }
 }
